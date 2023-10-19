0dcdc245ec94794285afbe40dceb2365ccb2ab24
RinadQudah <145657170+RinadQudah@users.noreply.github.com>
2023-10-19 14:51:02 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import 'bootstrap/dist/css/bootstrap.css'
+import Header from './Header'
+import Paragraph from './paragraph'
+import Card from './card'
+import Footer from './Footer'
+function App() {
+    return (
+    <>
+    <Header/>
+    <Paragraph/>
+    <Card/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
